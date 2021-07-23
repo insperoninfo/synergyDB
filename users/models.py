40a3c6c12520f76f4318eaecbb2534df7d476ee0
beingbiplov <beingbiplov@gmail.com>
2021-07-23 19:17:04 +0545

    add welcome mail

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from .send_emails import message_notification
 
 GENDER_CHOICES = (
 		("male", "M"),
@@ -30,6 +31,7 @@ class Profile(models.Model):
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
+        message_notification(instance.email,instance.first_name, instance.last_name)
         Profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
---
+++ users/send_emails.py
@@ -0,0 +1,24 @@
+from django.core.mail import EmailMessage
+from synergyDB.settings import DEFAULT_FROM_EMAIL, EMAIL_HOST_PASSWORD
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+
+def message_notification(r_email,first_name,last_name):
+
+	msg1 = f'Welcome { first_name } to Synergy management & Consultants.'
+	msg2 = f'If you have not received your login credentials please contact the administration.'
+	message = Mail(
+	    from_email=DEFAULT_FROM_EMAIL,
+	    to_emails=r_email,
+	    subject='Welcome Mail',
+	    html_content='<h4>'+ msg1 +'</h4>' + '<br><br>' + ' Please visit <a href="http://206.189.234.28/">http://206.189.234.28/</a> to Log In to the system. <br><br>' + msg2 )
+	try:
+	    sg = SendGridAPIClient(EMAIL_HOST_PASSWORD)
+	    response = sg.send(message)
+	    print(response.status_code)
+	    print(response.body)
+	    print(response.headers)
+	except Exception as e:
+	    print(e.message)
